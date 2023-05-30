1348cdde3d6b3e67026386b565d3d23eee4fb491
vinhhung263 <hungpv263@gmail.com>
2023-05-30 11:36:34 +0700

    update for project 5

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,5 +4,5 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   domain: 'dev-gar55nt1nhag3aeg.us.auth0.com',            // Auth0 domain
   clientId: 'UE6OT98w7SFuCHXWfXBKafnN8ainXsMF',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://project5-udacity-cloud-dev-hungpv15.s3-website-us-east-1.amazonaws.com/callback'
 }
